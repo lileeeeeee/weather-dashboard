e7c792f2f342adb892517ffb21349055b399b852
lileee <112982735+lileeeeeee@users.noreply.github.com>
2022-12-02 16:07:30 -0600

    set up fetch requests for getting city by lat/lon and extracting a five day forecast. Have not added current day forecast yet.

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,5 @@
 var APIKey = "f7c9249a4ca819afd8dd2f34c5b9766e";
 
-var queryURL = "http://api.openweathermap.org/data/2.5/forecast?lat="+ lat + "&lon=" + lon + "&appid=" + APIKey;
-
-var lat;
-var lon;
 
 var searchForm = document.querySelector('#searchBox');
 
@@ -20,14 +16,44 @@ function formSubmit(event) {
 //using only city for now, no state or country options//
 var queryURLGeocode = "http://api.openweathermap.org/geo/1.0/direct?q=" + userCity + "&limit=" + 1 + "&appid=" + APIKey;
 console.log(queryURLGeocode);
+//sets local storage to the city the user typed//
 localStorage.setItem("city", userCity)
- fetch(queryURLGeocode) 
- .then(function (response) {
-    console.log(response.json());
 
- })
+
+fetch(queryURLGeocode) 
+.then(response => response.json())
+.then(json => {
+     var lat = json[0].lat; 
+     var lon = json[0].lon;
+     console.log(lat);
+     console.log(lon);
+     var queryURL = "http://api.openweathermap.org/data/2.5/forecast?lat="+ lat + "&lon=" + lon + "&cnt=42&appid=" + APIKey;
+     console.log(queryURL);
+     return fetch(queryURL);
+})
+.then (function (response) {
+return response.json();
+})
+.then(function (data) {
+  console.log(data);
+  console.log(data.list[0]);
+  console.log(data.list[8]);
+  console.log(data.list[16]);
+  console.log(data.list[24]);
+  console.log(data.list[32]);
+});
+
 
   
 }
 
 searchForm.addEventListener('submit', formSubmit);
+
+
+// fetch(queryCrypto) .then(response => response.json())
+// .then(json => { 
+//     let coins = json.coins; 
+//     console.log(coins);
+//     console.log(coins[arrayPosition].item.id);
+
+// }); 
